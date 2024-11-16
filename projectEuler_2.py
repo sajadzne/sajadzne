181e32f76a59626cd3bfb17d2874ae00d9919b3a
sajadzne <sajadzeinodin835@gmail.com>
2024-11-16 15:46:56 +0330

    Create projectEuler_2.py

---
+++ projectEuler_2.py
@@ -0,0 +1,19 @@
+def itsEven(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+    
+first = 1
+second = 2 
+
+sum = 0
+
+while (first < 4000000):
+    if itsEven(first):
+        sum = sum + first
+    new = first + second
+    first = second
+    second = new
+
+print(sum)
